7c7b7dcc277abb56df252f4c7c14094b20da3a56
msimbisayi <msimbisayi@deloittece.com>
2023-08-07 17:18:05 +0200

    changed world class to a singleton and launched a robot successfully.

--- server/main.py
+++ server/main.py
@@ -1,11 +1,13 @@
-import world
-import position
-import robot
+from world import World
+from position import Position
+from robot import Robot
 
 
 def parse_request(data):
+    world = World.get_instance()
     if 'command' in data and data['command'] == "launch":
-        print(f"I've been asked to launch the robot {data['robot']}")
+        new_robot = Robot(data['robot'])
+        world.launch_robot(new_robot)
         return "Success, 201"
     else:
         return "Failure, 401"
--- server/world.py
+++ server/world.py
@@ -1,8 +1,21 @@
-import position
+from position import Position
+
+
 class World:
+    _instance = None
+
+    @staticmethod
+    def get_instance():
+        if World._instance is None:
+            World._instance = World()
+        return World._instance
+
     def __init__(self):
-        self.center = position((0, 0))
-        self.height = 100
-        self.width = 50
-        self.robots = []
-        self.obstacles = []
+        self._center = Position((0, 0))
+        self._height = 100
+        self._width = 50
+        self._robots = []
+        self._obstacles = []
+
+    def launch_robot(self, robot):
+        self._robots.append(robot)
