f4ea6c64130070b657dcc739802ba2d3be80a887
msimbisayi <msimbisayi@deloittece.com>
2023-09-06 13:46:10 +0200

    complete the state command along with its tests.

--- server/src/state.py
+++ server/src/state.py
@@ -8,14 +8,54 @@ class State(Command):
         super().__init__('state')
 
     def execute(self, request):
-        world = World.get_instance()
-        robot = world.get_robot(request["robot"])
-        position = robot.get_position()
-        state = {
-            "position": [position.x_coordinate, position.y_coordinate],
-            "direction": robot.get_direction(),
-            "shields": robot.get_shields(),
-            "shots": robot.get_shots(),
-            "status": robot.get_status()
+        if self.valid_request(request):
+            world = World.get_instance()
+            robot = world.get_robot(request["robot"])
+            if robot is not None:
+                position = robot.get_position()
+                state = {
+                    "position": [position.x_coordinate, position.y_coordinate],
+                    "direction": robot.get_direction(),
+                    "shields": robot.get_shields(),
+                    "shots": robot.get_shots(),
+                    "status": robot.get_status()
+                }
+                return state
+            else:
+                return self._invalid_robot_error()
+        else:
+            return self._invalid_request_error()
+
+    def valid_request(self, request):
+        """ Checks that the received request is valid"""
+
+        if "robot" not in request or request["robot"] is None:
+            return False
+        elif "command" not in request or request["command"] is None:
+            return False
+        elif "arguments" not in request or request["arguments"] is None:
+            return False
+        elif type(request["arguments"]) is not list or len(request["arguments"]) != 0:
+            return False
+        else:
+            return True
+
+    def _invalid_request_error(self):
+        """Returns an error response when the request is invalid"""
+
+        return {
+            "result": "ERROR",
+            "data": {
+                "message": "Your request is not configured properly"
+            }
         }
-        return state
+
+    def _invalid_robot_error(self):
+        """Returns an error response when the request contains a robot name that does not exist"""
+
+        return {
+            "result": "ERROR",
+            "data": {
+                "message": "Robot does not exist"
+            }
+        }
--- server/tests/test_state.py
+++ server/tests/test_state.py
@@ -17,10 +17,42 @@ def test_state_success():
     launch_response = launch_command.execute(test_data_robot)
     state_command = State()
     state_response = state_command.execute(test_data_state)
-
+    print(state_response)
     assert state_response["position"][0] == launch_response["state"]["position"][0]
     assert state_response["position"][1] == launch_response["state"]["position"][1]
     assert state_response["direction"] == launch_response["state"]["direction"]
     assert state_response["shields"] == launch_response["state"]["shields"]
     assert state_response["shots"] == launch_response["state"]["shots"]
     assert state_response["status"] == launch_response["state"]["status"]
+
+
+def test_invalid_request():
+    test_data_robot = {
+        "robot": "Tom_state",
+        "command": "launch",
+        "arguments": ["sniper", 5, 5]
+    }
+
+    test_data_state = {
+        "robot": "Tom_state",
+        "command": "state",
+        "arguments": [5]
+    }
+    launch_command = Launch()
+    launch_command.execute(test_data_robot)
+    state_command = State()
+    response = state_command.execute(test_data_state)
+    assert response["result"] == "ERROR"
+    assert response["data"]["message"] == "Your request is not configured properly"
+
+def test_invalid_robot():
+    test_data_state = {
+        "robot": "John_state",
+        "command": "state",
+        "arguments": []
+    }
+    state_command = State()
+    response = state_command.execute(test_data_state)
+    print(response)
+    assert response["result"] == "ERROR"
+    assert response["data"]["message"] == "Robot does not exist"
